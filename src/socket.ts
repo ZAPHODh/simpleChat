20c6fc92d55cbbbe539511a79e468f677818407b
Luis <luispaulo.ni@gmail.com>
2024-07-07 15:08:18 -0300

    addded new ngrok server

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,4 @@
 
 import { io } from 'socket.io-client'
 
-export const socket = io(
-    'https://e3a7-2804-d41-ef0b-7f00-6da9-ea3c-ec1a-f94f.ngrok-free.app/socket.io/'
-)
+export const socket = io('https://1c59-200-165-154-163.ngrok-free.app')
