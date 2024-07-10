cae02926cc10394034fd52902a5f3deb73f1f2cb
Luis <luispaulo.ni@gmail.com>
2024-07-10 15:44:51 -0300

    removed redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { Chat } from '@/components/Chat'
 import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
 
-export default async function Home() {
-    const session = await getServerSession()
-    if (!session || !session.user) redirect('api/auth/signin')
+export default function Home() {
     return (
         <main>
             <Chat room={'room1'} />
