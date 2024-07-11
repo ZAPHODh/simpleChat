d223c6a349e6de42bb4859795e893cf8624ad9fc
Luis <luispaulo.ni@gmail.com>
2024-07-11 01:09:24 -0300

    addded server to env

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,4 +1,4 @@
 'use client'
 import { io } from 'socket.io-client'
 
-export const socket = io('https://892a0a6d0cb37e79c5ff4cb3f9000506.serveo.net')
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER)
