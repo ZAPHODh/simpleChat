4056c28cee4fb5e6c03441e2c0c3c7ae83cd7b23
Luis <luispaulo.ni@gmail.com>
2024-07-11 10:15:40 -0300

    running server

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,4 +1,4 @@
 'use client'
 import { io } from 'socket.io-client'
 
-export const socket = io('https://6d6d50f8dbbf0909adf92fbfe1e3bc96.serveo.net')
+export const socket = io('https://f4f75e8ab8287da16ddec6584267af27.serveo.net')
