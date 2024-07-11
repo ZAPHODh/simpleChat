e55e4d1dc2119e2022a58464330c4525f10f937a
Luis <luispaulo.ni@gmail.com>
2024-07-10 21:32:00 -0300

    test 3

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,4 +1,4 @@
 'use client'
 import { io } from 'socket.io-client'
 
-export const socket = io('https://2866-201-9-69-21.ngrok-free.app')
+export const socket = io('https://892a0a6d0cb37e79c5ff4cb3f9000506.serveo.net')
